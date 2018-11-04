3e4407caea44cab72ffb463848a00f818d6c388d
Harsha Malireddy <harshamalireddy@1x-nat-vl931-172-30-180-4.wireless.umass.edu>
2018-11-04 11:58:33 -0500

    views push

--- src/UMassForum/UMassApp/views.py
+++ src/UMassForum/UMassApp/views.py
@@ -3,30 +3,43 @@ from django.http import HttpResponse
 from UMassApp.models import DiscussionPost, SurveyPost, UserAccount
 from django.urls import reverse
 from django.views import generic
-
-# Create your views here.
-def index(request):
+  
+# Create your views here.  
+def index(request): 
 	"""View function for home page of site."""
 	# Generate counts of some of the main objects
 	num_discposts = DiscussionPost.objects.all().count()
 	num_surveyposts = SurveyPost.objects.all().count()
 
-	print(num_surveyposts)
+	print(num_surveyposts) 
 	# The 'all()' is implied by default.
-	num_users = UserAccount.objects.count()
+	num_users = UserAccount.objects.count() 
 
-	context = {
+	context = { 
 	    "num_discposts": num_discposts,
-	    "num_surveyposts": num_surveyposts,
-	    "num_users": num_users,
-	}
+	    "num_surveyposts": num_surveyposts, 
+	    "num_users": num_users, 
+	}  
 
 	# Render the HTML template index.html with the data in the context variable
 	return render(request, "index.html", context=context)
+ 
+def createsurvey(request):
+	num_surveyposts = SurveyPost.objects.all().count()
+	context = {'num_surveyposts': num_surveyposts}
+	return render(request, 'UmassApp/createsurvey.html', context)
 
-
-class SurveyPostsView(generic.ListView):
+def surveyResults(request): #surveyResults(request,surveypost_id):
+	all_surveyposts = SurveyPost.objects.all()
+	context = {
+		'all_surveyposts': all_surveyposts, 
+		 
+		# "surveypost_id": surveypost_id,  
+	}   
+	# return HttpResponse("<h4>test: " + str(surveypost_id) + "</h4>")
+	return render(request, 'UmassApp/surveyresults.html', context) 
+  
+class SurveyPostsView(generic.ListView): 
     model = SurveyPost
-
+ 
     template_name = "surveypost.html"
-
