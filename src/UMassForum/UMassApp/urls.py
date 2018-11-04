b5b29929ad3e2a1875bbb008b773732830dc86de
Harsha Malireddy <harshamalireddy@1x-nat-vl931-172-30-180-4.wireless.umass.edu>
2018-11-04 12:02:11 -0500

    urls push

--- src/UMassForum/UMassApp/urls.py
+++ src/UMassForum/UMassApp/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
+urlpatterns = [ 
     path("", views.index, name='index'),
     path("surveyposts/", views.SurveyPostsView.as_view(), name="surveyposts"),
+    path("createsurvey/", views.createsurvey, name="createsurvey"),
+    path('surveyresults/', views.surveyResults, name="surveyresults"),
+    # path('surveyresults/<int:surveypost_id>/', views.surveyResults, name="surveyresults"),
 #     path('<int:pk>/', views.SPDetailsView.as_view(), name='spdetails'),
 #     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
 #     path('<int:surveypost_id>/vote/', views.vote, name='vote'),
-# 
+#  
 ]
+      
