97a4fb4b1e7f108cdf16c80d0d80170b62ae86b4
stulich <stephenulich@gmail.com>
2018-11-05 17:14:32 -0500

    Survey Detail View

--- src/UMassForum/UMassApp/urls.py
+++ src/UMassForum/UMassApp/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("discussionposts/", views.discussionPosts, name="discussionPosts"),
     path("surveyresults/", views.surveyResults.as_view(),name="survey_results"),
     path("discussioncreation/", views.createDiscussion, name="discussioncreation"),
+    path(r'^surveyresults/(?P<pk>[0-9A-Fa-f-]+)', views.SurveyPostsView.as_view(), name="survey-post-detail"),
 
 ]
       
