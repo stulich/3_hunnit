2ace983d3533b41b11ad08e6bdeb8d45df4874d1
Harsha Malireddy <harshamalireddy@1x-nat-vl931-172-30-148-14.wireless.umass.edu>
2018-11-27 14:08:14 -0500

    discussionpost update finished

--- src/UMassForum/UMassApp/urls.py
+++ src/UMassForum/UMassApp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from . import views
+from .import views
+import uuid
 
 urlpatterns = [ 
     path("", views.index, name='index'),
@@ -11,6 +12,13 @@ urlpatterns = [
     path("discussioncreation/", views.createDiscussion.as_view(), name="discussioncreation"),
     path(r'^surveyresults/(?P<pk>[0-9A-Fa-f-]+)', views.SurveyPostsView.as_view(), name="survey-post-detail"),
     path(r'^discussionposts/(?P<pk>[0-9A-Fa-f-]+)', views.discussionDetails.as_view(), name="discussion-post-detail"),
+    # path('discussionposts/uuid.id/', views.discussionDetails.as_view(), name="discussion-post-detail"),
     path( "user/", views.UserGeneralView.as_view(), name="userindividual" ),
-]
-      
+    path(r'^discussionposts/(?P<pk>[0-9A-Fa-f-]+)/update/', views.DiscussionUpdate.as_view(), name='discussionposts_update') 
+    # path('discussionposts/uuid.id/update/', views.DiscussionUpdate.as_view(), name='discussionposts_update') 
+    
+    # path('surveyposts/<int:pk>/update/', views.SurveyUpdate.as_view(), name='surveyposts_update'),  
+    # path('discussionposts/<int:pk>/delete/', views.DiscussionDelete.as_view(), name='discussionposts_delete'),   
+    # path('surveyposts/<int:pk>/delete/', views.SurveyDelete.as_view(), name='surveyposts_delete') 
+]        
+        
